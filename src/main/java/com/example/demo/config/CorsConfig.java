39ab2db12cffe91ad1c0af230675264e61f2559e
filipeavila7 <filipeavila076@gmail.com>
2026-05-13 12:07:30 -0300

    ajustando o cors para deploy

--- src/main/java/com/example/demo/config/CorsConfig.java
+++ src/main/java/com/example/demo/config/CorsConfig.java
@@ -16,7 +16,8 @@ public class CorsConfig {
         CorsConfiguration config = new CorsConfiguration();
         config.setAllowedOriginPatterns(List.of(
                 "http://localhost:*",
-                "http://127.0.0.1:*"
+                "http://127.0.0.1:*",
+                "https://*.railway.app"
         ));
         config.setAllowedMethods(List.of("GET", "POST", "PUT", "DELETE", "OPTIONS"));
         config.setAllowedHeaders(List.of("*"));
