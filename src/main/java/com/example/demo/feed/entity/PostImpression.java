326409bfbb1213e80d6271e065fca25eca8873fa
filipeavila7 <filipeavila076@gmail.com>
2026-08-17 10:35:45 -0300

    nova entidade de postImpression

---
+++ src/main/java/com/example/demo/feed/entity/PostImpression.java
@@ -0,0 +1,27 @@
+package com.example.demo.feed.entity;
+
+import com.example.demo.post.entity.Post;
+import com.example.demo.user.entity.User;
+import jakarta.persistence.*;
+
+import java.time.LocalDateTime;
+
+@Entity
+@Table(name = "post_impressions")
+public class PostImpression {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @ManyToOne
+    @JoinColumn(name = "user_id", nullable = false)
+    private User user;
+
+    @ManyToOne
+    @JoinColumn(name = "post_id", nullable = false)
+    private Post post;
+
+    @Column(nullable = false)
+    private LocalDateTime shownAt;
+}
