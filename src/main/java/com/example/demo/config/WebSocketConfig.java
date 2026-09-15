77dcd65cb6ebadc346ee6d66c9fc09ef93b2565a
filipeavila7 <filipeavila076@gmail.com>
2026-09-15 12:12:29 -0300

    modificações na configuração de webScocket

--- src/main/java/com/example/demo/config/WebSocketConfig.java
+++ src/main/java/com/example/demo/config/WebSocketConfig.java
@@ -13,8 +13,10 @@ public class WebSocketConfig implements WebSocketMessageBrokerConfigurer {
     @Override
     public void registerStompEndpoints(StompEndpointRegistry registry) {
         registry.addEndpoint("/ws")
-                .setAllowedOriginPatterns("http://localhost:5173", "http://127.0.0.1:5173", "*")
-                .withSockJS();
+                .setAllowedOriginPatterns(
+                        "http://localhost:5173",
+                        "http://127.0.0.1:5173"
+                );
     }
 
     @Override
